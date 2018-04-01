f9c5cd1b72d692a457985ab122187a24ea992ec5
firmansyah0218 <ferihabibi96@gmail.com>
2018-04-01 11:57:00 +0800

    Membuat Attribute dan Behavior

--- src/praktek02/penjualanlaptop.java
+++ src/praktek02/penjualanlaptop.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class penjualanlaptop {
+    int harga;
+    int jumlah;
     
+    void cetakInfo(){
+        System.out.println("===================");
+        System.out.println("Harga   :"+harga);
+        System.out.println("Jumlah  :"+jumlah);
+        System.out.println("===================");
+    }
 }
