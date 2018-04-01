fd6493de8d03ea0cfdf42c4b54efc72cc572c70b
firmansyah0218 <ferihabibi96@gmail.com>
2018-04-01 12:18:14 +0800

    Mengisi nilai attribute

--- src/praktek02/penjualanlaptop.java
+++ src/praktek02/penjualanlaptop.java
@@ -1,12 +1,12 @@
 package praktek02;
 public class penjualanlaptop {
-    int harga;
-    int jumlah;
+    int harga_laptop;
+    int jumlah_barang;
     
     void cetakInfo(){
         System.out.println("===================");
-        System.out.println("Harga   :"+harga);
-        System.out.println("Jumlah  :"+jumlah);
+        System.out.println("Harga Laptop  :"+harga_laptop);
+        System.out.println("Jumlah Barang :"+jumlah_barang);
         System.out.println("===================");
     }
 }
--- src/praktek02/penjualanlaptopaksi.java
+++ src/praktek02/penjualanlaptopaksi.java
@@ -3,5 +3,8 @@ package praktek02;
 public class penjualanlaptopaksi {
     public static void main(String[] args) {
         penjualanlaptop r1 = new penjualanlaptop();
+        
+        r1.harga_laptop = 4000000;
+        r1.jumlah_barang = 3;
     }
 }
